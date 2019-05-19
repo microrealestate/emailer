f84a60f1249c4d18f3bb1b62b2e45de02cf7e223
Camel Aissani <camel.aissani@nuageprive.fr>
2019-05-19 19:37:46 +0200

    added new email template

--- data/rentcall/index.js
+++ data/rentcall/index.js
@@ -5,27 +5,21 @@ const db = require('../');
 module.exports = {
     async get(params) {
         await db.start();
-        const data = {};
+
         const tenant = await db.getTenant(params.tenantId, params.term);
-        data.tenant = tenant;
-
         const locale = params.locale || 'fr';
-        data.billingRef = `${moment(params.term, 'YYYYMMDDHH').locale(locale).format('MM_YY')}_${tenant.reference}`;
-        data.filename = `${i18n(locale)[`short_${params.document}`]}-${tenant.name}-${data.billingRef}`;
-
         const momentTerm = moment(params.term, 'YYYYMMDDHH').locale(locale);
         const momentToday = moment().locale(locale);
-        data.period = momentTerm.format('MMMM YYYY');
-        data.today = momentToday.format('LL');
-
         const dueDate = moment(momentTerm).add(10, 'days');
         const dueDay = dueDate.isoWeekday();
+
         if ( dueDay === 6 ) {
             dueDate.subtract(1, 'days');
         } else if ( dueDay === 7 ) {
             dueDate.add(1, 'days');
         }
-        data.dueDate = dueDate.format('LL');
+
+        let billingDay = momentToday;
         if (dueDate.isSameOrBefore(momentToday)) {
             const today = moment(momentTerm);
             const day = today.isoWeekday();
@@ -34,8 +28,20 @@ module.exports = {
             } else if ( day === 7 ) {
                 today.add(1, 'days');
             }
-            data.today = today.format('LL');
+            billingDay = today;
         }
-        return data;
+
+        const data = {
+            tenant,
+            billingRef: `${moment(params.term, 'YYYYMMDDHH').locale(locale).format('MM_YY')}_${tenant.reference}`,
+            period: momentTerm.format('MMMM YYYY'),
+            today: billingDay.format('LL'),
+            dueDate: dueDate.format('LL')
+        };
+
+        return {
+            ...data,
+            filename: `${i18n(locale)[`short_${params.document}`]}-${tenant.name}-${data.billingRef}`
+        };
     }
 };
